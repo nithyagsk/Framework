24ab07b6ac1ab218ac86787d04ce7a54b19e9a1e
nithya <nithyagganesh@gmail.com>
2022-03-19 15:19:25 +0530

    Added testcases

--- Mavenhybrid/src/test/java/applicationpages/Apploginpage.java
+++ Mavenhybrid/src/test/java/applicationpages/Apploginpage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class Apploginpage {
 
@@ -21,16 +22,26 @@ public Apploginpage(WebDriver ldriver) {
 
 @FindBy(xpath = "//input[@value='Login']") WebElement loginbutton;
 
+@FindBy (xpath = "//div[text()=' Warning: No match for E-Mail Address and/or Password.']") WebElement alertmessage;
+@FindBy(linkText ="Forgotten Password") WebElement forgotpasswordlink;
+@FindBy(linkText = "Register") WebElement RmenuReglink;
+@FindBy(xpath = "//a[normalize-space()='login page']") WebElement Rpgloginlink;
+@FindBy(xpath = "//body/div[@id='account-register']/div[1]/aside[1]/div[1]/a[1]") WebElement Rmenuloginlink;
+@FindBy(xpath = "//a[@title='My Account']") WebElement MyaccountDropdown;
+@FindBy(xpath = "//ul[@class='dropdown-menu dropdown-menu-right']//a[normalize-space()='Login']") WebElement MyAcddloginlink;
+@FindBy(xpath ="//ul[@class='breadcrumb']//a[normalize-space()='Login']") WebElement Breadcrumbloginpage;
+
+
+
+
+
 public void Loginmethod(String usermail, String userpass) {
 	
 try {
 	Thread.sleep(2000);
 } catch (InterruptedException e) 
 
-{
-
-	
-	
+{	
 }	
 	
 emailfield.sendKeys(usermail);
@@ -39,6 +50,79 @@ loginbutton.click();
 
 
 }
+
+public WebElement alertmessage() {
+	
+ 	
+	return alertmessage;
+}
+
+public void forgotpasswordlink() {
+	
+ forgotpasswordlink.click();
+
+}
+public void LoginFromReg() {
+ 
+	RmenuReglink.click();
+	System.out.println(driver.getTitle());
+	Rpgloginlink.click();
+	Assert.assertEquals(driver.getTitle(),"Account Login");
+	
+	/*if(driver.getTitle()=="Account Login") {
+		Assert.assertTrue(true, "Not able to navigate to login page from registration page(login page link )");
+	}*/
+	
+	
+}
+public  void LoginFromRegRmenu() {
+	RmenuReglink.click();
+	Rmenuloginlink.click();
+	Assert.assertEquals(driver.getTitle(),"Account Login");
+	/*if(driver.getTitle()=="Account Login")
+    {
+    Assert.assertTrue(true, "not able to navigate to login page from registrationpage(right menu option)");
+}*/
+}
+
+public void LoginFromMyaccdropdown() {
+	
+	MyaccountDropdown.click();
+	MyAcddloginlink.click();
+
+	Assert.assertEquals(driver.getTitle(),"Account Login");
+	
+	/*if(driver.getTitle()=="Account Login")
+    {
+    Assert.assertTrue(true, "not able to navigate to login page from My AccountDrop down");
+}*/
+}
+
+public void Loginpagedetailsbreadcrmb() {
+	
+	Assert.assertEquals(Breadcrumbloginpage.getText(),"Login");
+  /*	if(Breadcrumbloginpage.getText()=="Login")
+	{
+		Assert.assertTrue(true, "not able to find login-breadcrumb");
+	}*/
+}
+
+public void Loginpagedetails() {
+	
+	Assert.assertEquals(driver.getTitle(),"Account Login");
+	
+	/*if(driver.getTitle()== "Account Login")
+{
+Assert.assertTrue(true, "not able to verify, mismatch in login page title");
+}*/
+	System.out.println("...driver.getCurrentUrl()"+driver.getCurrentUrl());
+  //
+	Assert.assertEquals(driver.getCurrentUrl(),"https://demo.opencart.com/index.php?route=account/login" );
+	
+	
+}
+
+
 
 
 }
--- Mavenhybrid/src/test/java/apptestcases/Applogintestcase.java
+++ Mavenhybrid/src/test/java/apptestcases/Applogintestcase.java
@@ -1,6 +1,10 @@
 package apptestcases;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import BasePackage.primaryclass;
@@ -17,6 +21,8 @@ public void LoginintoApp() {
 	 Apploginpage AP= PageFactory.initElements(driver, Apploginpage.class);
 	 AP.Loginmethod(Red.Readvalue("login", 0, 0),Red.Readvalue("login", 0, 1));
     logger.info("validate using valid credentials");
+    System.out.println(driver.getTitle());
+   Assert.assertEquals(driver.getTitle(), "My Account");
     logger.pass("Login successful ");
     
     
@@ -31,7 +37,8 @@ public void TC_002() {
 Apploginpage An= PageFactory.initElements(driver, Apploginpage.class);
 An.Loginmethod(Red.Readvalue("login", 1, 0),Red.Readvalue("login", 1, 1));
 logger.info("validate using invalid credentials");	
-
+System.out.println(An.alertmessage().getText());
+Assert.assertEquals(An.alertmessage().getText(), "Warning: No match for E-Mail Address and/or Password.");
 logger.info("passed  test for invalid credentials");
 }
 
@@ -42,11 +49,39 @@ public void TC_003() {
 	 // Apploginpage An = new Apploginpage(driver);
 	Apploginpage Am= PageFactory.initElements(driver, Apploginpage.class);
 	Am.Loginmethod(Red.Readvalue("login", 2, 0),Red.Readvalue("login", 2, 1));
-	logger.info("validate using invalid credentials");	
+	logger.info("validate with blank credentials");	
 
-	logger.info("passed  test for invalid credentials");	
+	logger.info("test passed for blank credentials");	
 	
 }
-
+  @Test(priority=4)
+public void TC_004()
+{
+	logger = report.createTest("applogintestcase3");
+	Apploginpage Aq= PageFactory.initElements(driver, Apploginpage.class);
+	Aq.forgotpasswordlink();
+	System.out.println(driver.getTitle());
+	Assert.assertEquals(driver.getTitle(),"Forgot Your Password?");
+	logger.info("validating forgot password link");
+}
+@Test(priority=5)
+public void TC_005()
+{
+	logger = report.createTest("applogintestcase4");
+	Apploginpage At = PageFactory.initElements(driver, Apploginpage.class);
+	At.LoginFromReg();
+	At.LoginFromRegRmenu();
+	At.LoginFromMyaccdropdown();
+	logger.info("validating different ways to navigate to login page");
+}
+@Test(priority=6)
+public void TC_006()
+{
+	logger = report.createTest("applogintestcase5");
+	Apploginpage AL = PageFactory.initElements(driver, Apploginpage.class);
+	AL.Loginpagedetailsbreadcrmb();
+	AL.Loginpagedetails();
+    logger.info("validating login page breadcrumb, page title, page url");
+} 
 
 }
