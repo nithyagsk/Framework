42d88622bba27b09a5528af4384071cc1f800596
nithya <nithyagganesh@gmail.com>
2022-03-22 21:37:53 +0530

    updated

--- Mavenhybrid/src/test/java/apptestcases/Applogintestcase.java
+++ Mavenhybrid/src/test/java/apptestcases/Applogintestcase.java
@@ -1,8 +1,5 @@
 package apptestcases;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
